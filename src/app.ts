3c32a40cf6e9745c063b12207684d344b8fbd697
Eronmonsele oaikhina <eronoaik@gmail.com>
2024-12-26 07:22:42 +0100

    Push to repo

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, TypeScript with Express!");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
